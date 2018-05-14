c2c9c09be54bba35d447bce520b2abf3d68c3bf1
William Cory <willcory10@gmail.com>
2018-05-14 06:09:13 -0700

    complete VoteType

---
+++ src/db/models/voteType.js
@@ -0,0 +1,12 @@
+export default (sequelize, DataTypes) => {
+  var VoteType = sequelize.define('VoteType', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {});
+  VoteType.associate = (models) => {
+    VoteType.hasMany(models.Vote);
+  }
+  return VoteType
+}
