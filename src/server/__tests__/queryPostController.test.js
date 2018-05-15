8cdb6744895a5e64e4bcdcd201aed925eca0e5a7
William Cory <willcory10@gmail.com>
2018-05-15 14:16:20 -0700

    complete and test the controller and route

---
+++ src/server/__tests__/queryPostController.test.js
@@ -0,0 +1,44 @@
+import queryPostController, { db } from '../controller/queryPost'
+
+afterAll(async (done) => {
+  await db.sequelize.close();
+  done();
+});
+
+const fakeQueryParams = {
+  sortBy: 'viewCount',
+  id: 1,
+  PostTypeId: 1
+};
+
+const req = {
+  body: '',
+  query: fakeQueryParams,
+}
+
+class Res {
+  status(status) {
+    this.statuscode = status;
+    console.log('status sent:', status);
+    return this;
+  }
+
+  send(message) {
+    this.sentMessage = message;
+    // console.log('new message', message);
+    return this;
+  }
+
+  json(message) {
+    return this.send(message);
+  }
+}
+
+test('should return a 200 with a good query', async (done) => {
+  const res = new Res();
+  await queryPostController(req, res);
+
+  expect(res.statuscode).toBe(200);
+
+  done()
+});
