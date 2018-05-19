daed538f7f43075da807d347222c0aca957c56f9
William Cory <willcory10@gmail.com>
2018-05-19 13:27:57 -0700

    trying to hack migrations

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,9 +1,9 @@
 const config = {
   "development": {
-    "username": "postgres",
-    "password": "G,iANp5g\"W3q&f#fL(Vm\"2Ks&",
-    "database": "catalyst_development",
-    "host": "127.0.0.1",
+    "username": "psujmmqgzgpfgo",
+    "password": "0b5962328e2ccef022bf1759be702e1d7f58e132416d72524b9b7bd4cbe34f6c",
+    "database": "dbiff713ut8it3",
+    "host": "ec2-54-243-54-6.compute-1.amazonaws.com",
     "port": 5432,
     "dialect": "postgres"
   },
@@ -21,9 +21,3 @@ const config = {
     "ssl": true
   }
 }
-
-if (process.env.DATABASE_URL) {
-  config.production.url = process.env.DATABASE_URL
-}
-
-module.exports = config;
