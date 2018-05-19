830d417d865d38312020844aa7c322d87fc4397d
William Cory <willcory10@gmail.com>
2018-05-19 11:16:51 -0700

    move back to config.json

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -8,10 +8,13 @@ const config = require(`${__dirname}/../config/config.js`)[env];
 const db = {};
 
 let sequelize;
-
-sequelize = new Sequelize(
-  config.database, config.username, config.password, config
-);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+  sequelize = new Sequelize(
+    config.database, config.username, config.password, config
+  );
+}
 
 fs
   .readdirSync(__dirname)
