6c73966d5583554f5acd02c84ba902e1e9710810
William Cory <willcory10@gmail.com>
2018-05-19 11:51:35 -0700

    add url

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -22,7 +22,10 @@ if (config.use_env_variable) {
 }*/
 
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    ...config,
+    url: process.env[config.use_env_variable],
+  });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
