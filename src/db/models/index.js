959d20e59999f5ec4663c97d08197d07b2c68366
William Cory <willcory10@gmail.com>
2018-05-19 11:22:01 -0700

    change js to json

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -4,7 +4,7 @@ import Sequelize from 'sequelize'
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.js`)[env];
+const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 
 let sequelize;
