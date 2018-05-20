9d239c47291dfe089ed0fadf8cf779bf12672150
Frederick Peavy <peavy.frederick@gmail.com>
2018-05-20 19:05:33 -0400

    Add ElasticSearch

---
+++ src/search/index.js
@@ -0,0 +1,17 @@
+import elasticsearch from 'elasticsearch'
+import domain from './config/config'
+
+const client = new elasticsearch.Client({
+  host: domain,
+  log: 'trace'
+});
+
+client.ping({
+  requestTimeout: 30000,
+}, function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
